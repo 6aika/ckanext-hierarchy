775bcf4cfa38e665c4a620d27b60bf28a40fdb05
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-01-26 12:41:46 +0200

    Remove unnecessary prints

--- ckanext/hierarchy/helpers.py
+++ ckanext/hierarchy/helpers.py
@@ -2,10 +2,7 @@ import ckan.plugins as p
 from pylons import tmpl_context as c
 
 def get_group_tree():
-    group_tree = p.toolkit.get_action('group_tree')({'type': 'organization'})
-    import pprint
-    pprint.pprint(group_tree)
-    return group_tree
+    return p.toolkit.get_action('group_tree')({'type': 'organization'})
 
 def get_group_tree_section():
     return p.toolkit.get_action('group_tree_section')({'id': c.group_dict.id, 'type': c.group_dict.type})
