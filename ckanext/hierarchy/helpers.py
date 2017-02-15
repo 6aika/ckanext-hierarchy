b71b9f569b29edd3067f5e84099bb232d838bf24
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-02-15 16:09:41 +0200

    Fix group_tree_section action call when viewing organization about-template

--- ckanext/hierarchy/helpers.py
+++ ckanext/hierarchy/helpers.py
@@ -6,7 +6,11 @@ def get_group_tree():
     return p.toolkit.get_action('group_tree')({'type': 'organization'})
 
 def get_group_tree_section():
-    return p.toolkit.get_action('group_tree_section')({'id': c.group_dict.id, 'type': c.group_dict.type})
+    data_dict = {
+        'id': c.group_dict['id'],
+        'type': c.group_dict['type']
+    }
+    return p.toolkit.get_action('group_tree_section')({}, data_dict)
 
 def show_organizations_without_datasets():
     return True if p.toolkit.asbool(config.get('ckanext.hierarchy.show_organizations_without_datasets')) == True else False
